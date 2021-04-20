86bae3066a15175f3312461ab9366663306b4dd8
Hysios Hu <hysios@gmail.com>
2021-04-20 18:00:11 +0800

    Feature: add response utils

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/hysios/utils
 
 go 1.15
+
+require (
+	github.com/fatih/structs v1.1.0
+	github.com/iancoleman/strcase v0.1.3
+)
---
+++ response/response.go
@@ -0,0 +1,66 @@
+package response
+
+import (
+	"encoding/json"
+	"net/http"
+	"reflect"
+
+	"github.com/fatih/structs"
+	"github.com/iancoleman/strcase"
+)
+
+var (
+	rStatus  = reflect.ValueOf("status")
+	rSuccess = reflect.ValueOf("success")
+)
+
+func Jsonify(w http.ResponseWriter, val interface{}) {
+	var (
+		b   []byte
+		v   = reflect.ValueOf(val)
+		err error
+	)
+	v = reflect.Indirect(v)
+
+	if v.Kind() == reflect.Map {
+		if v.CanSet() {
+			v.SetMapIndex(rStatus, rSuccess)
+			b, err = json.Marshal(val)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic("can't set map")
+		}
+	} else if v.Kind() == reflect.Struct {
+		m := structs.Map(val)
+		m["status"] = "success"
+
+		b, err = json.Marshal(m)
+		if err != nil {
+			panic(err)
+		}
+	} else if val == nil {
+		var m = map[string]interface{}{
+			"status": "success",
+		}
+		b, _ = json.Marshal(m)
+	} else {
+		panic("invalid type")
+	}
+	w.Write(b)
+}
+
+func camelCase(str string) string {
+	return strcase.ToLowerCamel(str)
+}
+
+func AbortErr(w http.ResponseWriter, status int, err error) {
+	w.WriteHeader(status)
+	var m = map[string]interface{}{
+		"status": "error",
+		"errors": err.Error(),
+	}
+	b, _ := json.Marshal(m)
+	w.Write(b)
+}
